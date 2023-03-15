3500a200ba62aa089a1c27131c5b7ed456a3985f
sakibmahbub <arafatsakib48@gmail.com>
2023-03-15 12:29:22 +0600

    fixed random rooms bug

--- taglineGenarator.js
+++ taglineGenarator.js
@@ -24,18 +24,18 @@ generateTag(tagLines, 3000);
 document
   .getElementById("random-room-btn")
   .addEventListener("click", async function () {
-    console.log("hello world");
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
+    const { name, summary, property_type, images, review_scores } =
+      result[randomIndex];
 
     const reviews = document.createElement("ol");
     if (review_scores.scores) {
       reviews.setAttribute("class", "list-group");
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores.scores).forEach((key) => {
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
@@ -58,12 +58,14 @@ document
         <p class="card-text">${summary}</p>
         <div id='review-score'>
       Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
-    </div>
+      <div id="reviews"></div>
+      </div>
       </div>
       <button class="btn btn-info btn-lg"  role="button"
                    >Show details</button>
     </div>
    </div>
     `;
+
+    document.getElementById("reviews").appendChild(reviews);
   });
